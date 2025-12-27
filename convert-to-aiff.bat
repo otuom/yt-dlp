95c1be520161893f8a635b25eb0c7c801c1414e4
Oscar Tuominen <oscar.tuominen@riksteatern.se>
2025-12-27 15:40:57 +0100

    Update convert-to-aiff.bat

--- convert-to-aiff.bat
+++ convert-to-aiff.bat
@@ -1,2 +1,2 @@
-FOR /F "tokens=*" %G IN ('dir /b *.opus') DO ffmpeg -i "%G" -map_metadata 0:s:0 -write_id3v2 1 -c:v copy "%~nG.aiff"
-FOR /F "tokens=*" %G IN ('dir /b *.m4a') DO ffmpeg -i "%G" -map_metadata 0 -write_id3v2 1 -c:v copy "%~nG.aiff"
+FOR /F "tokens=*" %G IN ('dir /b *.m4a') DO ffmpeg -i "%G" -ar 44100 -map_metadata 0 -write_id3v2 1 -c:v copy "%~nG.aiff"
+FOR /F "tokens=*" %G IN ('dir /b *.opus') DO ffmpeg -i "%G" -ar 44100 -map_metadata 0:s:0 -write_id3v2 1 -c:v copy "%~nG.aiff"
